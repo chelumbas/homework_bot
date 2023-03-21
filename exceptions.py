73bbc3288ec586d55047a0205cc6ce43591c7fbe
Dmitriy Chelumbasov <d.chelumbasov@gmail.com>
2023-03-21 18:43:31 +0300

    Change by review

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,13 @@
 class StatusCodeError(Exception):
     """Невалидный код ответа"""
     pass
+
+
+class RequestError(Exception):
+    """Ошибка запроса к сервису."""
+    pass
+
+
+class HomeworkStatusError(Exception):
+    """Новый статус домашней работы."""
+    pass
--- homework.py
+++ homework.py
@@ -1,30 +1,41 @@
-import telegram
-import time
-import requests
-import sys
-import os
 import logging
-from dataclasses import dataclass
-from typing import Optional
+import os
+import sys
+import time
 from http import HTTPStatus
-from exceptions import StatusCodeError
+
+import requests
+import telegram
 from dotenv import load_dotenv
+
+from exceptions import StatusCodeError, RequestError, HomeworkStatusError
+
 load_dotenv()
+logger = logging.getLogger(__name__)
 
-
-@dataclass(frozen=True)
-class LoggedMessages:
-    """Текст сообщения ошибок."""
-
-    empty_token = (
-        'Отсутствует один из обязательных токенов. '
-        'Необходимо проверить наличие для продолжения работы.'
-    )
-    incorrect_status = 'Запрос вернул некорректный статус.'
-    former_status = 'Статус работы не изменился.'
-    successful_message = 'Сообщение успешно отправлено.'
-    type_error = 'Получены данные в некорректного формата:'
-    key_error = 'Получена переменная со следующими ключами:'
+EMPTY_TOKEN = (
+    'Отсутствует один из обязательных токенов. '
+    'Необходимо проверить наличие для продолжения работы.'
+)
+REQUEST_ERROR = (
+    'Запросе к URL {url} с параметрами {params} '
+    'вернул ошибку {error}'
+)
+INCORRECT_STATUS = 'Запрос вернул некорректный статус {status}.'
+FORMER_STATUS = 'Статус работы не изменился.'
+SUCCESSFUL_MESSAGE = 'Сообщение успешно отправлено.'
+TYPE_ERROR = (
+    'Некорректный тип данных. Ожидался {expected_type}. '
+    'Был получен {received_type}.'
+)
+KEY_ERROR = (
+    'Отсутствует ключ необходимый ключ в словаре {dictionary}. '
+    'Доступные ключи {received_keys}.'
+)
+INDEX_ERROR = 'Получен пустой список {received_list}.'
+HOMEWORK_STATUS_ERROR = (
+    'Получен неизвестный статус {status} домашней работы. '
+    'Необходимо дополнить значения словаря {dictionary}')
 
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
@@ -43,22 +54,9 @@ HOMEWORK_VERDICTS = {
 }
 
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s: %(levelname)s: %(message)s: %(name)s',
-    handlers=[
-        logging.FileHandler("main.log", mode='w'),
-        logging.StreamHandler(sys.stdout)
-    ]
-)
-
-
 def check_tokens() -> bool:
     """Проверяет наличие токенов."""
-    if not PRACTICUM_TOKEN or not TELEGRAM_TOKEN:
-        logging.critical(LoggedMessages.empty_token)
-        return False
-    return True
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def send_message(bot, message):
@@ -66,11 +64,12 @@ def send_message(bot, message):
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
     except telegram.TelegramError as error:
-        logging.error(error)
-    logging.debug(LoggedMessages.successful_message)
+        logger.error(error)
+    else:
+        logger.debug(SUCCESSFUL_MESSAGE)
 
 
-def get_api_answer(timestamp: int) -> Optional[dict]:
+def get_api_answer(timestamp: int) -> dict:
     """Возвращает результат ревью."""
     payload = {'from_date': timestamp}
     try:
@@ -80,30 +79,44 @@ def get_api_answer(timestamp: int) -> Optional[dict]:
             headers=HEADERS
         )
     except requests.RequestException as request_error:
-        logging.warning(request_error)
-        return None
+        raise RequestError(
+            REQUEST_ERROR.format(
+                url=ENDPOINT, params=payload, error=request_error
+            )
+        )
     if response.status_code != HTTPStatus.OK:
         raise StatusCodeError(
-            LoggedMessages.incorrect_status,
-            ENDPOINT,
-            response.status_code,
-            response.text
+            INCORRECT_STATUS.format(status=response.status_code)
         )
     return response.json()
 
 
 def check_response(response: dict) -> dict:
     """Валидирует ответ от сервиса."""
+    print(response)
     if not isinstance(response, dict):
-        raise TypeError(LoggedMessages.type_error, type(response))
+        raise TypeError(
+            TYPE_ERROR.format(
+                expected_type=dict, received_type=type(response)
+            )
+        )
     homeworks = response.get('homeworks')
-    if not homeworks:
-        raise KeyError((LoggedMessages.key_error, response.keys()))
+    if homeworks is None:
+        raise KeyError(
+            KEY_ERROR.format(
+                dictionary='response', received_keys=response.keys()
+            )
+        )
     if not isinstance(homeworks, list):
-        raise TypeError(LoggedMessages.type_error, type(homeworks))
-    homework = homeworks[0]
-    if not homework:
-        raise ValueError
+        raise TypeError(
+            TYPE_ERROR.format(
+                expected_type=list, received_type=type(homeworks)
+            )
+        )
+    try:
+        homework = homeworks[0]
+    except IndexError:
+        raise IndexError(INDEX_ERROR.format(received_list='homeworks'))
     return homework
 
 
@@ -111,11 +124,18 @@ def parse_status(homework: dict) -> str:
     """Возвращает сообщение с результатом ревью."""
     status = homework.get('status')
     homework_name = homework.get('homework_name')
-    if (
-            not status or status not in HOMEWORK_VERDICTS.keys()
-            or not homework_name
-    ):
-        raise KeyError(LoggedMessages.key_error, homework.keys())
+    if status is None or homework_name is None:
+        raise KeyError(
+            KEY_ERROR.format(
+                dictionary='homework', received_keys=homework.keys()
+            )
+        )
+    if status not in HOMEWORK_VERDICTS:
+        raise HomeworkStatusError(
+            HOMEWORK_STATUS_ERROR.format(
+                status=status, received_keys=HOMEWORK_VERDICTS
+            )
+        )
     verdict = HOMEWORK_VERDICTS.get(status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -123,6 +143,7 @@ def parse_status(homework: dict) -> str:
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
+        logger.critical(EMPTY_TOKEN)
         sys.exit()
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
@@ -134,21 +155,30 @@ def main():
     while True:
         try:
             response = get_api_answer(timestamp)
+            timestamp = response.get('current_date')
             homework = check_response(response)
             status = parse_status(homework)
             if status != last_homework_status:
-                last_homework_status = status
                 send_message(bot, status)
+                last_homework_status = status
             else:
-                logging.debug(LoggedMessages.former_status)
+                logger.debug(FORMER_STATUS)
         except Exception as error:
             message = f'Сбой в работе программы: {repr(error)}'
             if message != last_error_message:
-                last_error_message = message
                 send_message(bot, message)
+                last_error_message = message
         finally:
             time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s: %(levelname)s: %(message)s: %(name)s',
+        handlers=[
+            logging.FileHandler("main.log", mode='w'),
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
     main()
